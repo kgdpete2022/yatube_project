5392214894261b98093b1843860d229a3b222502
Пётр Шопин <mail4dev2022@gmail.com>
2022-09-08 23:58:57 +0200

    Добавлены urls и view-функции

---
+++ yatube/posts/views.py
@@ -0,0 +1,10 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse('Главная страница')
+
+def group_posts(request, slug):
+    return HttpResponse(f'Посты, отфильтрованные по группам: {slug}')
